f4745e00e0bc56c099f6cc9dc07e161db219d2a8
DevAriane <arianeateumo@gmail.com>
2026-09-15 01:54:48 +0100

    fix: recreation propre du fichier

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/presentation/views/Home.vue";
 import AddMenuItem from "@/presentation/views/AddMenuItem.vue";
-import updateMenuitem from "@/presentation/views/UpdateMenuitem.vue";
+import Update from "@/presentation/views/Update.vue";
 import OrderMenuItem from "@/presentation/views/OrderMenuItem.vue";
 
 const routes = [
     { path: '/', component: Home },
     { path: '/addMenuItem', component: AddMenuItem },
-    { path: '/updateMenuItem/:id', component: updateMenuitem },
+    { path: '/updateMenuItem/:id', component: Update },
     { path: '/orderMenuItem', component: OrderMenuItem }
 ];
 
