01e7ebfd986914e57afc49e16d1a21bd93a673bb
DevAriane <arianeateumo@gmail.com>
2026-09-23 09:11:23 +0100

    add correect

---
+++ src/presentation/views/DetailsMenuItem.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import ActionMenu from './component/DetailsMenuItem/ActionMenu.vue';
+import Infos from './component/DetailsMenuItem/Infos.vue';
+
+</script>
+<template></template>
